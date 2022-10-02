73bbe7fb61ecf9c224dd47f4c93df2351b58388e
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-10-02 21:59:05 +0530

    st3 training done

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -1,10 +1,10 @@
-import argparse
 import os
-import shutil
-from tqdm import tqdm
+import joblib
+import argparse
 import logging
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from src.utils.common import read_yaml, create_directories
-import random
 
 
 STAGE = "Three" 
@@ -22,7 +22,33 @@ def main(config_path, params_path):
     config = read_yaml(config_path)
     params = read_yaml(params_path)
     
+    artifacts = config["artifacts"]
+    featurized_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
+    featurized_train_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TRAIN"])
 
+    model_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["MODEL_DIR"])
+    create_directories([model_dir_path])
+    model_path = os.path.join(model_dir_path, artifacts["MODEL_NAME"])
+
+    matrix = joblib.load(featurized_train_data_path)
+
+    labels = np.squeeze(matrix[:, 1].toarray())
+    X = matrix[:,2:]
+
+    logging.info(f"input matrix size: {matrix.shape}")
+    logging.info(f"X matrix size: {X.shape}")
+    logging.info(f"Y matrix size or labels size: {labels.shape}")
+
+    seed = params["train"]["seed"]
+    n_est = params["train"]["n_est"]
+    min_split = params["train"]["min_split"]
+
+    model = RandomForestClassifier(
+        n_estimators=n_est, min_samples_split=min_split, n_jobs=2, random_state=seed
+    )
+    model.fit(X, labels)
+
+    joblib.dump(model, model_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
